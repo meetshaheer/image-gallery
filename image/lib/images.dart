8dc251780c9d221c84dc1f06132a0f5b13b4bd28
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-05-06 18:32:25 +0500

    updated wit hcolumn properties

--- image/lib/images.dart
+++ image/lib/images.dart
@@ -9,12 +9,15 @@ class images extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Image.network(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEshvOHzmvJUZ-6X8mQ3bNIjCNSPIeek_xFRF1kGR9BA&s'),
-            Image.asset('assets/images/car1.jpg')
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEshvOHzmvJUZ-6X8mQ3bNIjCNSPIeek_xFRF1kGR9BA&s'),
+              Image.asset('assets/images/car1.jpg'),
+              Image.asset('assets/images/car2.jpg'),
+            ],
+          ),
         ),
       ),
     );
